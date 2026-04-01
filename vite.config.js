a155e3530705f28755ea7ce298d203d7ba82d532
Tejas0606 <chardetejas1@gmail.com>
2026-04-01 15:11:30 +0530

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  // Must match your GitHub repository name exactly
-  base: "/daily-motivation-dashboard/",
-});
